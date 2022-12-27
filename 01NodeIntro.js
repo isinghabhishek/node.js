e8043284ee4eab84e0a9a2a83757c1ef9fbe58a0
Abhishek Singh <singhabhishek3006.abhi@gmail.com>
2022-12-27 19:24:57 +0530

    first commit

---
+++ 01NodeIntro.js
@@ -0,0 +1,28 @@
+// What is Node js?
+// Node is not a Languare.
+// This is a Server Environment.
+// Node js can connect with database.
+// Code and syntax is very similar to JavaScript.
+// But Not Exactly same.
+// Node js is free, open-source.
+// Node js use "Chrome's V8 engine" to execute code.
+
+// Why do we use Node?
+// Node js mostly used for API
+// so we can connect the same database with Web App, Mobile App.
+// Node is easy to understatnd who know javaScript.
+// Node is super-fast for APIs
+// with Node and JavaScript, you can become full statck Developer.
+
+// JavaScript & Node are the same?
+// JavaScript and Node js Code Syntax is Same
+// If you Know javaScript you can easily understand Node
+// But both are not exactly the same
+// you can not connect javaScript to DB
+// Node can connect with DB
+// Node js run on the server side and JavaScript is run on the Browser
+
+// What developer Make with Node js>
+// Developer Make API with Node js
+// So we can connect server with client
+// Node can make API for WEB, ANDROID and IOS etc. 
---
+++ 02index.js
@@ -0,0 +1,28 @@
+const Export = require('./03Export'); // 
+
+var a=20;
+let b=30;
+const c=40;
+console.warn(a+b+c);
+
+// Diffrences b/w "==" && "==="
+// "==" -> for value
+// "===" -> for value and type both
+
+// importing data from another file
+// console.log(Export);
+// console.log(Export.x); // importing indivisual values
+// console.log(Export.z()); // importing function
+
+// filter
+// with help of filter function we can perform especific operation
+// and it will return a new arr and we can store it.
+const arr = [2, 6, 6, 8, 10, 12];
+let result = arr.filter((item) => {
+    return item*2;
+})
+console.warn(result);
+
+// Nodemon
+// helps in runing the coding in live-time
+// by pressing ctrl+S code will be asscicute
