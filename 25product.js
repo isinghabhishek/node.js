e4738a8d3f15790394d3ec74374efd30e78de804
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2023-05-31 22:05:50 +0530

    add: files

---
+++ 25product.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
+    name:String,
+    price:Number,
+    brand:String,
+    category:String
+});
+
+module.exports = mongoose.model('product', productSchema);
