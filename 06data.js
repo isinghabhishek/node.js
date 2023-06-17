75cf23b6bf232f9527f36f4ef764e4cf51ea851d
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2023-06-18 01:17:22 +0530

    Updata changes in file

--- 06data.js
+++ 06data.js
@@ -4,6 +4,7 @@ const data = [
     {name:'Abhishek Singh', email:'abhishek@singh.com'},
     {name:' Singh', email:'abhishek@singh.com'},
     {name:'Abhishek ', email:'abhishek@singh.com'},
-    {name:'AbhiSingh', email:'abhishek@singh.com'}
+    {name:'AbhiSingh', email:'abhishek@singh.com'},
+    {name: 'Prithvi', email: 'prithivi@gmail.com'}
 ]
 export default data;
