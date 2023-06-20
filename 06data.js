7ad9366b6a8f629ece7b8f850f6df709ffd516d1
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2023-06-20 22:14:15 +0530

    Update: file

--- 06data.js
+++ 06data.js
@@ -5,6 +5,7 @@ const data = [
     {name:' Singh', email:'abhishek@singh.com'},
     {name:'Abhishek ', email:'abhishek@singh.com'},
     {name:'AbhiSingh', email:'abhishek@singh.com'},
-    {name: 'Prithvi', email: 'prithivi@gmail.com'}
+    {name: 'Prithvi', email: 'prithivi@gmail.com'},
+    {name: 'Ashutosh', email: 'ashu@gmail.com'}
 ]
 export default data;
